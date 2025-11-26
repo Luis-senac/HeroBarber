714db4e1db2aaa5a1cdb5fb97e419ef6c11e1a87
Luis-senac <senacluis658@gmail.com>
2025-11-26 16:43:58 -0300

    API

--- api.js
+++ api.js
@@ -1,12 +1,12 @@
 const API_BASE = "/api";
 
 export async function fetchAgendamentos() {
-  const res = await fetch(`${API_BASE}/agendamentos`);
+  const res = await fetch(`${API_BASE}/agendamento`);
   return await res.json();
 }
 
 export async function salvarAgendamento(agendamento) {
-  const res = await fetch(`${API_BASE}/agendamentos`, {
+  const res = await fetch(`${API_BASE}/agendamento`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(agendamento),
